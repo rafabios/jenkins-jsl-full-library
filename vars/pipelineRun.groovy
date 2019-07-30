67d836f309561076033de956c85f96a32d077e75
Rafael Souza <rafabios@gmail.com>
2019-07-30 15:27:55 -0300

    renamed 00-pipelineRun

---
+++ vars/pipelineRun.groovy
@@ -0,0 +1,32 @@
+
+
+def execute() {
+
+  node {
+
+    stage('Initialize') {
+      checkout scm
+      echo 'Loading pipeline definition'
+      Yaml parser = new Yaml()
+      Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yml').text)
+      // def pipelineDefinition = 50-utilsPipeline()  // Testar
+    }
+
+    switch(pipelineDefinition.pipelineType) {
+      case 'python':
+        // Instantiate and execute a Python pipeline
+        new pythonPipeline(pipelineDefinition).executePipeline()
+      case 'dotnet':
+        // Instantiate and execute a DotNet pipeline
+        new dotnetPipeline(pipelineDefinition).executePipeline()
+      case 'front':
+        // Instantiate and execute a Front pipeline
+        new frontPipeline(pipelineDefinition).executePipeline()
+      case 'job':
+        // Instantiate and execute a Job pipeline
+        new jobPipeline(pipelineDefinition).executePipeline()
+    }
+
+  }
+
+}
