630064b0accf9a0320b25a3390264a277c8d21cc
Rafael Souza <rafabios@gmail.com>
2019-07-31 14:14:20 -0300

    change lib

--- vars/pipelineRun.groovy
+++ vars/pipelineRun.groovy
@@ -16,7 +16,7 @@ def call() {
     switch(pipelineDefinition.type) {
       case 'python':
         // Instantiate and execute a Python pipeline
-        new pythonPipeline(pipelineDefinition).executePipeline()
+        new pythonPipeline(pipelineDefinition)
       //case 'dotnet':
         // Instantiate and execute a DotNet pipeline
         //new dotnetPipeline(pipelineDefinition).executePipeline()
