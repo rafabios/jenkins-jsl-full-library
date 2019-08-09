358ee0adc54e079270bfe24f2b685926885e1f64
Rafael Souza <rafabios@gmail.com>
2019-08-09 17:29:00 -0300

    CHANGED BUILD IMAGE TO VAR

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -5,7 +5,7 @@ def call() {
     def v = varsPipeline()
 
 podTemplate(containers: [
-  containerTemplate(name: 'python-template', image: 'python:latest', ttyEnabled: true, command: 'cat')
+  containerTemplate(name: 'python-template', image: "${env.JENKINS_DOCKER_BUILD_IMAGE}", ttyEnabled: true, command: 'cat')
   ]) {
 
   node(POD_LABEL) {
