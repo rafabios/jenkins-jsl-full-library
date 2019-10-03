5bd1d283fc27e504853a79a4b5725a60c44f5004
Rafael Souza <rafabios@gmail.com>
2019-10-03 16:18:49 -0300

    adicionado env nas variaveis do Jenkins

--- vars/approvallPipeline.groovy
+++ vars/approvallPipeline.groovy
@@ -3,17 +3,17 @@ def call() {
         stage('Aguardando aprovação') {
             echo 'aprovacao'
             try {
-            if ("${BRANCH_NAME}" == 'master' || "${BRANCH_NAME}" == 'release') {
+            if ("${env.BRANCH_NAME}" == 'master' || "${env.BRANCH_NAME}" == 'release') {
                         timeout(time:2, unit:'DAYS') {
-                            input message:"Aprovar o Deploy em produção?( ${BRANCH_NAME}  )", submitter: 'admin'
+                            input message:"Aprovar o Deploy em produção?( ${env.BRANCH_NAME}  )", submitter: 'admin'
                         }
 
             } else {
-                println "Branch:  ${BRANCH_NAME} nao precisa de aprovacao!"
+                println "Branch:  ${env.BRANCH_NAME} nao precisa de aprovacao!"
                 println 'Seguindo com Deploy'
                 }
             } catch (Exception e) {
-                       sh "Erro ao identificar a Branch ${BRANCH_NAME}"
+                       sh "Erro ao identificar a Branch ${env.BRANCH_NAME}"
              }    
 
             }
