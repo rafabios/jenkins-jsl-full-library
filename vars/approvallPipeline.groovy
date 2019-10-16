fc5399170c24902bd261faafad4cd4cc64faa388
Rafael Souza <rafabios@gmail.com>
2019-10-16 11:37:28 -0300

    ADD def for branch_name

--- vars/approvallPipeline.groovy
+++ vars/approvallPipeline.groovy
@@ -1,22 +1,24 @@
 def call(String BRANCH_NAME) {
         
+        def BRANCH_NAME_DEF = BRANCH_NAME
 
         stage('Aguardando aprovação') {
             def v = varsPipeline()
+            
 
             try {
-            if ("${BRANCH_NAME}" == 'master' || "${BRANCH_NAME}" == 'release') {
+            if ("${BRANCH_NAME_DEF}" == 'master' || "${BRANCH_NAME_DEF}" == 'release') {
                         timeout(time:2, unit:'DAYS') {
-                            input message:"Aprovar o Deploy em produção?( ${BRANCH_NAME}  )", submitter: 'admin'
+                            input message:"Aprovar o Deploy em produção?( ${BRANCH_NAME_DEF}  )", submitter: 'admin'
                         }
 
             } else {
-                println "Branch:  ${BRANCH_NAME} nao precisa de aprovacao!"
+                println "Branch:  ${BRANCH_NAME_DEF} nao precisa de aprovacao!"
                 println 'Seguindo com Deploy'
-                println "${v.mPROJETO_NAME}"
+                println "${v.mPROJETO_NAME_DEF}"
                 }
             } catch (Exception e) {
-                       sh "Erro ao identificar a Branch ${mBRANCH_NAME}"
+                       sh "Erro ao identificar a Branch ${mBRANCH_NAME_DEF}"
              }    
 
             }
